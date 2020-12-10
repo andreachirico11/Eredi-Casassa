32e5f89c4d5390debc820fa886a8926183ec6141
andreachirico11 <andreachirico11@gmail.com>
2020-12-10 12:13:01 +0100

    fixed nav button

--- javascripts/navbar.js
+++ javascripts/navbar.js
@@ -1,15 +1,3 @@
-const navbarOpen = 'navbar-open',
-  displayNone = 'display-none',
-  navbar = document.getElementsByClassName('navbar')[0];
-
-const openBtn = document.getElementsByClassName('navbar-button-open')[0];
-const closeBtn = document.getElementsByClassName('navbar-button-close')[0];
-const addClickList = (callb, el) => {
-  el.addEventListener('click', callb);
-};
-const toggleOpenClose = () => {
-  navbar.classList.toggle(navbarOpen);
-  openBtn.classList.toggle(displayNone);
-};
-addClickList(toggleOpenClose, openBtn);
-addClickList(toggleOpenClose, closeBtn);
+document.getElementById('navbar-toggler').addEventListener('click', () => {
+  document.getElementsByTagName('header')[0].classList.toggle('navbar-open');
+});
