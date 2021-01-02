d4a1c21c84b3dea80c4ec557dfac8dbfc03fc5fa
andreachirico11 <andreachirico11@gmail.com>
2021-01-02 13:47:40 +0100

    map completed

---
+++ javascripts/map.js
@@ -0,0 +1,24 @@
+let gmap;
+const coo = {
+  lat: 44.3177348,
+  lng: 9.3241951,
+};
+const url =
+  'https://www.google.it/maps/place/Casassa+Eredi+(S.A.S.)/@44.317653,9.3219863,17z/data=!3m1!4b1!4m5!3m4!1s0x12d4987fc9d96917:0x20c2196c4468ac76!8m2!3d44.317653!4d9.324175';
+function initMap() {
+  gmap = new google.maps.Map(document.getElementsByClassName('gMap')[0], {
+    center: coo,
+    zoom: 16,
+    streetViewControl: false,
+    zoomControl: false,
+    mapTypeControl: false,
+  });
+  const mk = new google.maps.Marker({
+    position: coo,
+    map: gmap,
+    title: 'Eredi Casassa',
+  });
+  google.maps.event.addListener(mk, 'click', () => {
+    window.location = url;
+  });
+}
