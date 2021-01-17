139fe6f1b44ad8c9394eaba78259c4fda3dc5f39
Andrea Chirico <andreachirico11@gmail.com>
2021-01-17 18:49:27 +0100

    read from db completed

--- public/javascripts/category.js
+++ public/javascripts/category.js
@@ -2,6 +2,20 @@ const categoryTemplate = document.getElementById('category-template');
 const categoryParam = window.location.href.split('?')[1];
 const container = document.getElementById('products-container');
 const url = '../products/products.json';
+const database = firebase.database();
+
+database.ref(categoryParam).on(
+  'value',
+  (fetchedProducts) => {
+    if (!fetchedProducts) {
+      return redirectToProducts();
+    }
+    addCategoryProductsToView(fetchedProducts.val());
+  },
+  (err) => {
+    redirectToProducts();
+  }
+);
 
 function configureProduct(product) {
   const category = categoryTemplate.content.cloneNode(true);
@@ -11,10 +25,9 @@ function configureProduct(product) {
   return category;
 }
 
-function addCategoryProductsToView(category) {
-  const products = category.products;
-  if (products.length === 0) {
-    noProductFound();
+function addCategoryProductsToView(products) {
+  if (!products || products.length === 0) {
+    return noProductFound();
   }
   products.forEach((prod) => {
     container.appendChild(configureProduct(prod));
@@ -23,36 +36,47 @@ function addCategoryProductsToView(category) {
 
 function noProductFound() {
   const template = document.createElement('h1');
+  template.id = 'noProdFound';
   template.textContent = 'No Product Found';
   container.appendChild(template);
-}
-
-function extractCategoryFromDb(db) {
-  return db.filter((category) => category.categoryName === categoryParam)[0];
 }
 
 function redirectToProducts() {
   window.location.href = window.location.origin + '/products.html';
 }
 
-window.onload = () => {
-  fetch(url)
-    .then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-      throw new Error();
-    })
-    .then((db) => {
-      if (db) {
-        const category = extractCategoryFromDb(db);
-        if (!category) {
-          redirectToProducts();
-        }
-        addCategoryProductsToView(category);
-      }
-    })
-    .catch((err) => {
-      redirectToProducts();
-    });
-};
+// function addCategoryProductsToView(category) {
+//   const products = category.products;
+//   if (products.length === 0) {
+//     noProductFound();
+//   }
+//   products.forEach((prod) => {
+//     container.appendChild(configureProduct(prod));
+//   });
+// }
+
+// function extractCategoryFromDb(db) {
+//   return db.filter((category) => category.categoryName === categoryParam)[0];
+// }
+
+// window.onload = () => {
+//   fetch(url)
+//     .then((res) => {
+//       if (res.ok) {
+//         return res.json();
+//       }
+//       throw new Error();
+//     })
+//     .then((db) => {
+//       if (db) {
+//         const category = extractCategoryFromDb(db);
+//         if (!category) {
+//           redirectToProducts();
+//         }
+//         addCategoryProductsToView(category);
+//       }
+//     })
+//     .catch((err) => {
+//       redirectToProducts();
+//     });
+// };
---
+++ public/javascripts/firebase-init.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: 'AIzaSyB_FWDsMuqiD-vQIJBWoxl7-MTbXWnnerA',
+  authDomain: 'eredicasassa.firebaseapp.com',
+  databaseURL:
+    'https://eredicasassa-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'eredicasassa',
+  storageBucket: 'eredicasassa.appspot.com',
+  messagingSenderId: '180018356989',
+  appId: '1:180018356989:web:a941b8613289b0d8001494',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
