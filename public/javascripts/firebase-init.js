05a4bf8c5236e21d641399d595d33807ac0a70e7
andreachirico11 <andreachirico11@gmail.com>
2021-12-01 07:01:57 +0100

    config adjust

--- public/javascripts/firebase-init.js
+++ public/javascripts/firebase-init.js
@@ -1,12 +1,4 @@
 // Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: googleApiKey,
-  authDomain: 'eredicasassa.firebaseapp.com',
-  databaseURL: 'https://eredicasassa-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'eredicasassa',
-  storageBucket: 'eredicasassa.appspot.com',
-  messagingSenderId: '180018356989',
-  appId: '1:180018356989:web:a941b8613289b0d8001494',
-};
+var firebaseConfig = Object.assign({}, FIREBASE_CONFIG);
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
